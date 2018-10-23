f85a12bf4753aac09843066506826d04d1fe4347
RushianHaker <ivanov2001m352g@mail.ru>
2018-10-23 12:47:26 +0300

    Initial Commit

---
+++ To Do List/ToDo.swift
@@ -0,0 +1,30 @@
+//
+//  ToDo.swift
+//  To Do List
+//
+//  Created by student on 23.10.2018.
+//  Copyright © 2018 Postnikov George. All rights reserved.
+//
+
+import Foundation
+
+struct ToDo {
+    var title: String
+    var isComplete: Bool
+    var dueDate: Date
+    var notes: String
+    
+    static func loadToDos() -> [ToDo]?{
+        return nil
+    }
+    
+    static func loadSampleToDos() -> [ToDo] {
+        return [
+            ToDo(title: "Дело 1", isComplete: false, dueDate: Date(), notes: "Заметка 1"),
+            ToDo(title: "Дело 2", isComplete: false, dueDate: Date(), notes: "Заметка 2"),
+            ToDo(title: "Дело 3", isComplete: false, dueDate: Date(), notes: "Заметка 3"),
+            ToDo(title: "Дело 4", isComplete: false, dueDate: Date(), notes: "Заметка 4"),
+        ]
+    }
+}
+
